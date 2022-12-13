23cf8429fd903f59bc84dacc67459b0313301a25
Talha Satır <imnever0729@gmail.com>
2022-12-14 00:51:18 +0300

    -bootstrap ve jquery kütüphaneleri projeye eklendi - kütüphanelere göre gerkli düzeltmeler gerçekleştirildi. - ana layout bootstrap ile temel tasarımı gerçekleşti.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare var $:any //jquery'in başarılı bir şekilde projeye dahil edildiğini anlayabiliyorum jqueryin doları ile.
 
 @Component({
   selector: 'app-root',
@@ -8,3 +9,6 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'ETicaretClient';
 }
+$(document).ready(()=>{ //sayfa açıldığında jquery ile alert basıyoruz.
+  alert("asd")
+})
